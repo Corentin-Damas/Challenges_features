134942bd48e77b9de08fbacaec0bd6171c5e4596
Nazemrap <corentin.damas@gmail.com>
2023-06-07 17:37:57 +0200

    interactive pricing global

--- Interactive pricing/script.js
+++ Interactive pricing/script.js
@@ -1,44 +1,80 @@
 const value = document.querySelector("#pageviews-value");
 const price = document.querySelector("#pageviews-price");
 const input = document.querySelector("#pageviews");
-// const slider = document.querySelector('.range-slider');
 const slider = document.querySelector(".range-slider");
+const bill = document.querySelector(".yearlybill-checkbox");
 
-value.textContent = "100k";
-price.textContent = "$16.00";
+// Base set up properties
+const valueDictionnary = {
+  prices: [8, 12, 16, 24, 36],
+  pageViews: ["10K", "50K", "100K", "500K", "1M"],
+};
+let range = 3;
 
+//function to DISPLAY values depending on Monthly / Yearly discount
+const applyValues = function (x) {
+  value.textContent = valueDictionnary["pageViews"][x];
+
+  let tempPrice = valueDictionnary["prices"][x];
+  bill.checked == true
+    ? (tempPrice = reducePrice(tempPrice))
+    : (tempPrice = tempPrice);
+  price.textContent = `$${tempPrice}.00`;
+};
+
+// Display the price when opening the page
+applyValues(range);
+
+// Function to calculate the yearly bill discount
+const reducePrice = function (coast) {
+  newPrice = coast - coast * 0.25;
+  return newPrice;
+};
+
+///////////////////////////////////////////////////////////
+// PRICE VALUE EVENT LISTENNER
+
+//  Refresh the price after using Switch button
+//  ~~ Fix problem where the prices is not refresh after mooving from range to range and
+//  ~~ suddenly swithing from on billing option to the other
+bill.addEventListener("click", () => {
+  applyValues(range);
+});
+
+//  Refresh the price after passing each treshold
 input.addEventListener("input", () => {
   let resist = document.querySelector("#pageviews").value;
 
   if (resist < 25) {
-    value.textContent = "10K";
-    price.textContent = "$8.00";
+    range = 0;
+    applyValues(range);
   }
 
   if (resist >= 25 && resist < 50) {
-    value.textContent = "50K";
-    price.textContent = "$12.00";
+    range = 1;
+    applyValues(range);
   }
 
   if (resist >= 50 && resist < 75) {
-    value.textContent = "100K";
-    price.textContent = "$16.00";
+    range = 2;
+    applyValues(range);
   }
 
   if (resist >= 75 && resist < 100) {
-    value.textContent = "500K";
-    price.textContent = "$24.00";
+    range = 3;
+    applyValues(range);
   }
 
   if (resist >= 100) {
-    value.textContent = "1M";
-    price.textContent = "$36.00";
+    range = 4;
+    applyValues(range);
   }
-
-  //   let sliderColor = `background-image: linear-gradient(90deg, var(--pr--soft-cyan) ${resist}%, var(--nt-light-grayish-blue-1) ${resist}%);`;
-  // slider.style.background = sliderColor;
 });
 
+///////////////////////////////////////////////////////////
+// CUSTOM CURSOR
+// ~~ We need to apply custom gradient for the cursor depending of
+// ~~ the possition from the cursor
 input.addEventListener("input", function () {
   let x = input.value;
   let color =
